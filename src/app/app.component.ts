4b20803be85d166608c6784f3a89f40bd4dafeb6
Yadiana Iraola <yadiana.molina@gmail.com>
2025-08-27 20:15:26 -0400

    Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { FlabFooterComponent } from './components/flab-footer/flab-footer.component';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { FlabHeaderComponent } from './components/flab-header/flab-header.component';
@@ -15,11 +16,23 @@ import { FlabTeamComponent } from './components/flab-team/flab-team.component';
     FlabHeaderComponent, 
     FlabHomeComponent, 
     FlabResearchComponent,
-    FlabTeamComponent
+    FlabTeamComponent,
+    FlabFooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'lab-research-admin';
+  scrolled = false;
+
+  ngAfterViewInit() {
+    const body = document.querySelector('body');
+    if (body) {
+      body.addEventListener('scroll', () => {
+        this.scrolled = body.scrollTop > 0;
+      });
+    }
+  }
+
 }
---
+++ src/app/components/flab-footer/flab-footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+
+@Component({
+  selector: 'app-flab-footer',
+  standalone: true,
+  imports: [NzGridModule],
+  templateUrl: './flab-footer.component.html',
+  styleUrl: './flab-footer.component.scss'
+})
+export class FlabFooterComponent {
+
+}
