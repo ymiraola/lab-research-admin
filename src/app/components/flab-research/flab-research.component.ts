3df9ae6e2baedb44be5f776f6bab076f07328f24
Yadiana Iraola <yadiana.molina@gmail.com>
2025-08-29 17:15:18 -0400

    Responsive Design II

--- src/app/components/flab-research/flab-research.component.ts
+++ src/app/components/flab-research/flab-research.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @Component({
   selector: 'app-flab-research',
   standalone: true,
-  imports: [NzCardModule],
+  imports: [NzCardModule, NzGridModule],
   templateUrl: './flab-research.component.html',
   styleUrl: './flab-research.component.scss'
 })
