474668742d14c6eccff1cd94298adc035020c2d7
Yadiana Iraola <yadiana.molina@gmail.com>
2025-08-29 14:53:28 -0400

    Header Responsive

--- src/app/components/flab-header/flab-header.component.ts
+++ src/app/components/flab-header/flab-header.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 
 @Component({
   selector: 'app-flab-header',
   standalone: true,
-  imports: [NzMenuModule, NzButtonModule],
+  imports: [NzMenuModule, NzButtonModule, NzDrawerModule],
   templateUrl: './flab-header.component.html',
   styleUrl: './flab-header.component.scss'
 })
 export class FlabHeaderComponent {
+
+  menuDrawerVisible = false;
+
+  closeMenuDrawer() {
+    this.menuDrawerVisible = false;
+  }
+
+  openMenuDrawer() {
+    this.menuDrawerVisible = true;
+  }
 
 }
